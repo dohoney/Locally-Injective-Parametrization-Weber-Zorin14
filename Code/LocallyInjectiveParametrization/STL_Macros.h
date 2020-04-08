a9ea293614f20f8dd9095f8a01540b8cba5866ef
dohoney <du.xingyi@wustl.edu>
2020-04-07 23:23:03 -0500

    delete former src code folder

--- Code/LocallyInjectiveParametrization/STL_Macros.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-
-typedef std::complex<double> ComplexDouble;
-typedef std::complex<long double> ComplexLongDouble;
-typedef std::complex<float> ComplexFloat;
-typedef ComplexDouble Complex;
